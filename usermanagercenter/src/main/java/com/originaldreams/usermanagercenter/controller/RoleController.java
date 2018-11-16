f44ae685c0122a80d3af4a131c2aa362ee984bce
yangkaile <guyexing@foxmail.com>
2018-11-16 20:49:46 +0800

    修改Controller名称

--- usermanagercenter/src/main/java/com/originaldreams/usermanagercenter/controller/RoleController.java
+++ usermanagercenter/src/main/java/com/originaldreams/usermanagercenter/controller/RoleController.java
@@ -4,10 +4,8 @@ import com.originaldreams.common.response.MyResponse;
 import com.originaldreams.common.router.MyUserManagerRouter;
 import com.originaldreams.common.router.RouterAttribute;
 import com.originaldreams.usermanagercenter.entity.Role;
-import com.originaldreams.usermanagercenter.entity.RoleRouters;
 import com.originaldreams.usermanagercenter.entity.UserRoles;
 import com.originaldreams.usermanagercenter.service.RoleService;
-import com.originaldreams.usermanagercenter.service.RouterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -18,20 +16,56 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- * 用户权限管理控制
- * 提供用户权限相关操作功能（包括角色划分、角色分配、授权等）
+ *  角色管理
  * @author 杨凯乐
- * @date 2018-07-30 09:18:22
+ * @date 2018-07-30 09:18:05
  */
 @RestController
-@RequestMapping("/permissionManager")
-public class PermissionManagerController {
-    private Logger logger = LoggerFactory.getLogger(PermissionManagerController.class);
+@RequestMapping("/role")
+public class RoleController {
+    private Logger logger = LoggerFactory.getLogger(RoleController.class);
 
     @Resource
     private RoleService roleService;
-    @Resource
-    private RouterService routerService;
+
+
+    /**
+     * 查询所有角色
+     * @return
+     */
+    @RouterAttribute(id = MyUserManagerRouter.GET_ALL_ROLES, description = "获取所有角色")
+    @RequestMapping(value = "/getAllRoles" , method = RequestMethod.GET)
+    public ResponseEntity getAllRoles(){
+        return MyResponse.ok(roleService.getAll());
+    }
+
+    /**
+     * 查询用户的角色
+     * @param userId
+     * @return
+     */
+    @RouterAttribute(id = MyUserManagerRouter.GET_ROLE_BY_USER_ID, description = "获取用户角色")
+    @RequestMapping(value = "/getRoleByUserId" , method = RequestMethod.GET)
+    public ResponseEntity getRoleByUserId(Integer userId){
+        if(userId == null || userId < 0){
+            return MyResponse.badRequest();
+        }
+        return MyResponse.ok(roleService.getRoleByUserId(userId));
+    }
+
+    /**
+     * 查询包含某个权限的角色
+     * @param routerId
+     * @return
+     */
+    @RouterAttribute(id = MyUserManagerRouter.GET_ROLES_BY_ROUTER_ID, description = "查询拥有某权限的角色")
+    @RequestMapping(value = "/getRolesByRouterId" , method = RequestMethod.GET)
+    public ResponseEntity getRolesByRouterId(Integer routerId){
+        if(routerId == null || routerId < 0){
+            return MyResponse.badRequest();
+        }
+        return MyResponse.ok(roleService.getRolesByRouterId(routerId));
+    }
 
     /**
      * 添加角色
@@ -64,23 +98,6 @@ public class PermissionManagerController {
         UserRoles userRoles = new UserRoles(userId, roleId);
         return MyResponse.ok(roleService.addRoleForUser(userRoles));
     }
-
-    /**
-     * 为角色添加权限
-     * @param roleId
-     * @param routerId
-     * @return
-     */
-    @RouterAttribute(id = MyUserManagerRouter.ADD_ROUTER_FOR_ROLE, description = "我是登录接口")
-    @RequestMapping(value = "addRouterForRole" , method = RequestMethod.POST)
-    public ResponseEntity addRouterForRole(Integer roleId,Integer routerId){
-        if(roleId == null || routerId == null){
-            return MyResponse.badRequest();
-        }
-        RoleRouters roleRouters = new RoleRouters(roleId,routerId);
-        return MyResponse.ok(routerService.addRouterForRole(roleRouters));
-    }
-
     /**
      * 删除角色
      * 这个接口要慎重调用
@@ -116,6 +133,4 @@ public class PermissionManagerController {
         }
         return MyResponse.ok(roleService.update(role));
     }
-
-
 }
--- usermanagercenter/src/main/java/com/originaldreams/usermanagercenter/controller/UserController.java
+++ usermanagercenter/src/main/java/com/originaldreams/usermanagercenter/controller/UserController.java
@@ -5,6 +5,7 @@ import com.originaldreams.common.router.MyUserManagerRouter;
 import com.originaldreams.common.router.RouterAttribute;
 import com.originaldreams.usermanagercenter.entity.UserInfo;
 import com.originaldreams.usermanagercenter.service.UserInfoService;
+import com.originaldreams.usermanagercenter.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author 杨凯乐
@@ -22,8 +22,10 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/userInfo")
-public class UserInfoController {
-    private Logger logger = LoggerFactory.getLogger(UserInfoController.class);
+public class UserController {
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Resource
+    private UserService userService;
 
     @Resource
     private UserInfoService userInfoService;
@@ -33,7 +35,7 @@ public class UserInfoController {
      * @param userId
      * @return
      */
-    @RouterAttribute(id = MyUserManagerRouter.GET_USER_INFO_BY_ID, description = "我是登录接口")
+    @RouterAttribute(id = MyUserManagerRouter.GET_USER_INFO_BY_ID, description = "获取用户信息")
     @RequestMapping(method = RequestMethod.GET)
     ResponseEntity get(Integer userId){
         if(userId == null){
@@ -41,11 +43,33 @@ public class UserInfoController {
         }
         return MyResponse.ok(userInfoService.getById(userId));
     }
-    @RouterAttribute(id = MyUserManagerRouter.UPDATE_USER_INFO, description = "我是登录接口")
+    @RouterAttribute(id = MyUserManagerRouter.UPDATE_USER_INFO, description = "修改用户信息")
     @RequestMapping(method = RequestMethod.PUT)
     ResponseEntity put(UserInfo userInfo){
         Integer result = userInfoService.update(userInfo);
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(result);
+    }
+
+
+    /**
+     * 查询拥有某个角色的用户
+     * @param roleId
+     * @return
+     */
+    @RouterAttribute(id = MyUserManagerRouter.GET_USER_BY_ROLE_ID, description = "查询拥有某角色的用户")
+    @RequestMapping(value = "/getUsersByRoleId" , method = RequestMethod.GET)
+    public ResponseEntity getUsersByRoleId(Integer roleId){
+        if(roleId == null || roleId < 0){
+            return MyResponse.badRequest();
+        }
+        return MyResponse.ok(userService.getUsersByRoleId(roleId));
+    }
+
+
+    @RouterAttribute(id = MyUserManagerRouter.GET_ALL_USER_NAME_AND_ROLE_NAME, description = "获取所有用户和对应的角色")
+    @RequestMapping(value = "/getAllUserNameAndRoleName" ,method = RequestMethod.GET)
+    public ResponseEntity getAllUserNameAndRoleName(){
+        return MyResponse.ok(userService.getAllUserNameAndRoleName());
     }
 
 }
